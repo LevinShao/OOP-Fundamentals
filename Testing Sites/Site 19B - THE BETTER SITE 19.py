665690e47b87a870719d42d74c3c5ddf1beecf64
Pixel Lemons <levin.shao@education.nsw.gov.au>
2025-08-27 21:55:20 +1000

    e

--- Testing Sites/Site 19B - THE BETTER SITE 19.py
+++ Testing Sites/Site 19B - THE BETTER SITE 19.py
@@ -1,5 +1,5 @@
-subjects = []
-days = []
+subjects = ["English", "Software", "Drama", "Science", "Sport"]
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
 
 def list_subjects(subjects):
     for index, subject in enumerate(subjects):
@@ -7,12 +7,13 @@ def list_subjects(subjects):
 
 def list_day(days):
     for day in days:
-        print(days)
+        print(day)
 
 list_day(days)
-print("margin")
+print("---------------------")
 
 for i in range(5):
     list_subjects(subjects)
     subjects.append(subjects[0])
     subjects.remove(subjects[0])
+    print("----------------------------")
